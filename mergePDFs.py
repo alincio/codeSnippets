a88891f937cfbeb92a17ca73a0f703ddd1f6e7f3
Alin Ciovica <alin.ciovica@gmail.com>
2022-11-07 14:17:49 +0200

    Merge PDFs - selective pages for a given pdf name

---
+++ mergePDFs.py
@@ -0,0 +1,20 @@
+#Inspiration topic: https://stackoverflow.com/questions/3444645/merge-pdf-files
+#This code merges all .pdf files in execution path and pages 1 and 5 of filenames containing "Gestiune"
+
+import os
+from PyPDF2 import PdfFileMerger
+from datetime import datetime
+
+x = [a for a in os.listdir() if a.endswith(".pdf")]
+print (x)
+merger = PdfFileMerger()
+for pdf in x:
+    #print (pdf.__contains__("Gestiune"))
+    if (pdf.__contains__("Gestiune")):
+     merger.append(open(pdf, 'rb'), pages=(0,5,4))
+    else:
+     merger.append(open(pdf, 'rb'))
+
+timestring = datetime.today().strftime('%d%m%Y')
+with open("MergedPDFs_" + timestring + ".pdf", "wb") as fout:
+ merger.write(fout)
